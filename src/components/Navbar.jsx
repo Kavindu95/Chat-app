1885bd788f2e67eef6cfb3398e5918d760699834
Kavindu95 <kavindueng95@gmail.com>
2023-12-10 12:32:12 +0530

    implement search component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Add from '../img/10.jpg'
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <span className="logo">Chat</span>
       <div className="user">
-        <img alt="" />
-        <span>user</span>
+        <img src={Add} alt="" />
+        <span>Kavindu</span>
         <button>logout</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Add from '../img/10.jpg'
 
 const Search = () => {
   return (
-    <div>
+    <div className='search'>
+      <div className='searchForm'>
+        <input type="text" name="" id="" />
+      </div>
+      <div className='userChat'>
+        <img src={Add} alt="" />
+        <div className="userChatInfo">
+          <span>Kavi</span>
+        </div>
+
+      </div>
       
     </div>
   )
